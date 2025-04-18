6869be07bdd876eccc6c49cf8b09c08a914d59e7
VimalCodezee <vimal.codezee@gmail.com>
2025-04-18 21:35:10 +0530

    bug fixes

--- src/Pages/Connections/Connections.jsx
+++ src/Pages/Connections/Connections.jsx
@@ -11,103 +11,107 @@ import { GlobalState } from '../../main'
 import Navbar from "../../Shared/Navbar";
 const Connections = () => {
     const api = useAPI();
-    const [items, setItems] = useState([]);
     const [activeModalState, setActiveModalState] = useContext(ActiveModal);
     const [currentState, setCurrentState] = useContext(GlobalState);
     const id = localStorage.getItem("id");
-    const fetch = useCallback(async () => {
-        const data = await api.getREQUEST(`getConnections/${id}`);
-        setItems(data);
+    const { data } = api.useGetRequest({
+      PATH: `getConnections/${id}`,
+      enabled: true,
+      initialData: [],
+      key: ["connection"],
     });
-    useEffect(() => {
-        fetch();
-    }, []);
+
     return (
-        <>
-            <div className={css.body}>
-                <Navbar left={`Hello 👋 ${currentState?.HRDetail?.Name}!`} />
-                <Tab
-                    tabName={`Connections`}
-                />
-                <div className={css.TableContainer}>
-                    <table class="table table-responsive-md  align-middle mb-0 ">
-                        <thead class="bg-light">
-                            <tr className="w-25">
-                                <th className="w-25 fs-5 fw-bold">{items.length} {items.length >= 1 ? " Connection " : " Connections "}Found</th>
-                            </tr>
-                            <tr>
-                                <th className="">Name</th>
-                                <th className="">Location</th>
-                                <th className="">Profession</th>
-                                <th className="">Actions</th>
-                            </tr>
-                        </thead>
-                        {items.length > 0 ?
-                            items.map((e) => {
-                                return (
-                                    <tbody>
-                                        <tr>
-                                            <td>
-                                                <div class="d-flex align-items-center">
-                                                    <img
-                                                        src={`${e.profileImage}`}
-                                                        alt=""
-                                                        onError={e => e.target.src = "https://w7.pngwing.com/pngs/695/655/png-transparent-head-the-dummy-avatar-man-tie-jacket-user.png"}
-                                                        style={{
-                                                            width: "45px",
-                                                            height: "45px",
-                                                        }}
-                                                        class="rounded-circle"
-                                                    />
-                                                    <div class="ms-3">
-                                                        <p class="fw-bold mb-1">
-                                                            {e.firstName}{" "}
-                                                            {e.lastName}
-                                                        </p>
-                                                        <p class="text-muted mb-0">
-                                                            {e.email}
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td>
-                                                <p class="fw-normal mb-1 text-nowrap ">
-                                                    {e.location &&
-                                                        e?.location[0]?.city},
-                                                    {e.location &&
-                                                        e?.location[0]?.state}
-                                                </p>
-                                            </td>
-                                            <td>{e.profession}</td>
-                                            <td className="">
-                                                <button
-                                                    type="button"
-                                                    onClick={() => {
-                                                        setActiveModalState("profileViewOfConnections")
-                                                        localStorage.setItem("connectionId", JSON.stringify(e))
-                                                    }
-                                                    }
-                                                    class="btn btn-outline-secondary   btn-rounded"
-                                                >
-                                                    Profile
-                                                </button>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                );
-                            }) :
-                            <thead>
-                            <tr>
-                                <td colSpan={5} className="text-center">
-                                    <span className="text-center fs-4 font-monospace"><i class="fa-regular fa-face-frown text-danger "></i>No Connections Found!</span>
-                                </td>
-                            </tr>
-                            </thead>
-                        }
-                    </table>
-                </div>
-            </div>
-        </>
+      <>
+        <div className={css.body}>
+          <Navbar left={`Hello 👋 ${currentState?.HRDetail?.Name}!`} />
+          <Tab tabName={`Connections`} />
+          <div className={css.TableContainer}>
+            <table class="table table-responsive-md  align-middle mb-0 ">
+              <thead class="bg-light">
+                <tr className="w-25">
+                  <th className="w-25 fs-5 fw-bold">
+                    {data?.length}{" "}
+                    {data?.length >= 1 ? " Connection " : " Connections "}Found
+                  </th>
+                </tr>
+                <tr>
+                  <th className="">Name</th>
+                  <th className="">Location</th>
+                  <th className="">Profession</th>
+                  <th className="">Actions</th>
+                </tr>
+              </thead>
+              {data?.length > 0 ? (
+                data.map((e) => {
+                  return (
+                    <tbody>
+                      <tr>
+                        <td>
+                          <div class="d-flex align-items-center">
+                            <img
+                              src={`${e.profileImage}`}
+                              alt=""
+                              onError={(e) =>
+                                (e.target.src =
+                                  "https://w7.pngwing.com/pngs/695/655/png-transparent-head-the-dummy-avatar-man-tie-jacket-user.png")
+                              }
+                              style={{
+                                width: "45px",
+                                height: "45px",
+                              }}
+                              class="rounded-circle"
+                            />
+                            <div class="ms-3">
+                              <p class="fw-bold mb-1">
+                                {e.firstName} {e.lastName}
+                              </p>
+                              <p class="text-muted mb-0">{e.email}</p>
+                            </div>
+                          </div>
+                        </td>
+                        <td>
+                          <p class="fw-normal mb-1 text-nowrap ">
+                            {e.location && e?.location[0]?.city},
+                            {e.location && e?.location[0]?.state}
+                          </p>
+                        </td>
+                        <td>{e.profession}</td>
+                        <td className="">
+                          <button
+                            type="button"
+                            onClick={() => {
+                              setActiveModalState("profileViewOfConnections");
+                              localStorage.setItem(
+                                "connectionId",
+                                JSON.stringify(e)
+                              );
+                            }}
+                            class="btn btn-outline-secondary   btn-rounded"
+                          >
+                            Profile
+                          </button>
+                        </td>
+                      </tr>
+                    </tbody>
+                  );
+                })
+              ) : (
+                <thead>
+                  <tr>
+                    <td colSpan={5} className="text-center">
+                      <span className="text-center fs-4 font-monospace">
+                        <i class="fa-regular fa-face-frown text-danger "></i>No
+                        Connections Found!
+                      </span>
+                    </td>
+                  </tr>
+                </thead>
+              )}
+            </table>
+          </div>
+        </div>
+      </>
     );
 };
 
--- src/Pages/Login/SignIn.jsx
+++ src/Pages/Login/SignIn.jsx
@@ -18,88 +18,92 @@ const SignIn = () => {
         setFormData({ ...formData, [name]: value });
     };
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        if (formData.email.length >= 2 && formData.password.length >= 8) {
-
-            try {
-                const response = await api.postREQUEST("Clogin", JSON.stringify(formData))
-                if (response.token) {
-                    localStorage.setItem("token", response.token)
-                    localStorage.setItem("id", response.data._id)
-                    toast.success("Login Successfully.")
-                    setScreen("root");
-                }
-                else {
-                    toast.error(response.error)
-                    setFormData({
-                        email: "",
-                        password: "",
-                    });
-                }
-            }
-            catch (e) {
-                toast.error(e);
-                setFormData({
-                    email: "",
-                    password: "",
-                });
-            }
+    const { mutate } = api.usePostREQUEST({
+      PATH: "Clogin",
+      onSuccess: (response) => {
+        if (response?.token) {
+          localStorage.setItem("token", response.token);
+          localStorage.setItem("id", response.data._id);
+          toast.success("Login Successfully.");
+          setScreen("root");
+        } else {
+          toast.error(response?.error);
+          setFormData({
+            email: "",
+            password: "",
+          });
         }
-        else{
-            toast.warning("provide email and passowrd")
-            setFormData({
-                email: "",
-                password: "",
-            });
-        }
+      },
+      onError:(error)=>{
+        toast.error(error?.response?.data?.message);
+      }
+    });
 
-        
+    const handleSubmit = () => {
+      if (formData.email.length >= 2 && formData.password.length >= 8) {
+        mutate(JSON.stringify(formData));
+      } else {
+        toast.warning("provide email and passowrd");
+        setFormData({
+          email: "",
+          password: "",
+        });
+      }
     };
 
     return (
-        <div className={css.main}>
-            <form onSubmit={handleSubmit} className={css.form}>
-                <div className={`${css.formHeader}`}>Sing In</div>
-                <div className={`${css.formSlogan} ${css.mt}`}>Step into the realm of security, where each login is a shield against digital intrusion.</div>
-                <div className={`${css.row} ${css.mt}`}>
-                    <input
-                        type="email"
-                        className={css.formInput}
-                        placeholder="Email"
-                        onChange={handleChange}
-                        required
-                        name="email"
-                        value={formData.email}
-                    />
-                </div>
-                <div className={`${css.row} ${css.mt}`}>
-                    <input
-                        type="password"
-                        className={css.formInput}
-                        placeholder="Password"
-                        required
-                        name="password"
-                        onChange={handleChange}
-                        value={formData.password}
-                    />
-                </div>
-                <div className={`${css.row} ${css.mt}`}>
-                    <Button type="text" text={"Login"} />
-                </div>
-                <div className={`${css.row} ${css.mt}`}>
-                    <span className={css.formFooterText}>
-                        Don't have an account ? <b className={css.formLink} onClick={() => setScreen("signup")}>Sign Up</b>
-                    </span>
-                </div>
-                <div className={`${css.row} mt-1`}>
-                    <span className={css.formFooterText2}>Forgot Password ? </span>
-                </div>
-                <span className="text-center small">
-                By clicking Agree & Join, you agree to the LinkedIn <span className="text-primary-emphasis ">User Agreement</span> and <span className="text-primary-emphasis ">Privacy Policy.</span>
-                </span>
-            </form>
+      <div className={css.main}>
+        <div className={css.form}>
+          <div className={`${css.formHeader}`}>Sing In</div>
+          <div className={`${css.formSlogan} ${css.mt}`}>
+            Step into the realm of security, where each login is a shield
+            against digital intrusion.
+          </div>
+          <div className={`${css.row} ${css.mt}`}>
+            <input
+              type="email"
+              className={css.formInput}
+              placeholder="Email"
+              onChange={handleChange}
+              required
+              name="email"
+              value={formData.email}
+            />
+          </div>
+          <div className={`${css.row} ${css.mt}`}>
+            <input
+              type="password"
+              className={css.formInput}
+              placeholder="Password"
+              required
+              name="password"
+              onChange={handleChange}
+              value={formData.password}
+            />
+          </div>
+          <div className={`${css.row} ${css.mt}`}>
+            <button className={css.botton} onClick={() => handleSubmit()} type="text">
+              Login
+            </button>
+          </div>
+          <div className={`${css.row} ${css.mt}`}>
+            <span className={css.formFooterText}>
+              Don't have an account ?{" "}
+              <b className={css.formLink} onClick={() => setScreen("signup")}>
+                Sign Up
+              </b>
+            </span>
+          </div>
+          <div className={`${css.row} mt-1`}>
+            <span className={css.formFooterText2}>Forgot Password ? </span>
+          </div>
+          <span className="text-center small">
+            By clicking Agree & Join, you agree to the LinkedIn{" "}
+            <span className="text-primary-emphasis ">User Agreement</span> and{" "}
+            <span className="text-primary-emphasis ">Privacy Policy.</span>
+          </span>
         </div>
+      </div>
     );
 };
 
